4050f04d8f5250be0db98edc2c4ce8d520f39ea8
Greg Slepak <contact@taoeffect.com>
2015-01-20 15:27:55 -0800

    Pulled out headers and footers and included them nginx-style & more
    + See README.md for how to view site locally
    + Added favicon.ico back
    + Added og header stuff for social sharing back
    + Fixed up some links (i think)

--- config.rb
+++ config.rb
@@ -3,10 +3,10 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+css_dir = "includes/css"
+sass_dir = "includes/sass"
+images_dir = "includes/images"
+javascripts_dir = "includes/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
