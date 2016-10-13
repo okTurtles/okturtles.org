c52b7d5219b2cbb07c19ca856d6ae92f59ba8a1c
Greg Slepak <contact@taoeffect.com>
2016-10-12 22:09:19 -0700

    Got rid of Google/YouTube + many cleanups/improvements
    - Replaced YouTube embed with local video-js (Closes #32)
    - Added .sass-lint.yml for sass linter support
    - Fixed video dimensions
    - Removed extraneous duplicate `includes/css` directory
    - Removed rogue `includes/.sass-cache` directory and ignored the other one
    - Load jQuery from our server
    - Added underscores to scss files that were not standalone files
    - Created high rez poster frame
    - Fixed `config.rb` to work with compass
    - Compactified generated css

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-okturtles.com
-=============
+# okturtles.com
 
 This project uses [nginx SSI](http://nginx.org/en/docs/http/ngx_http_ssi_module.html)
 to include header and footer files. For local testing it uses the
@@ -15,9 +14,16 @@ To test locally:
 
         npm install
 
-
 Then run the server using:
 
     coffee server.coffee
 
 And visit: [http://localhost:8080](http://localhost:8080)
+
+## Development
+
+To compile scss files `gem install compass` and then `compass compile` or `compass watch`.
+
+## TODO
+
+- Use a Gruntfile instead of this `coffee` and `compass` stuff.
--- config.rb
+++ config.rb
@@ -3,19 +3,20 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "includes/css"
+css_dir = "includes/stylesheets"
 sass_dir = "includes/sass"
 images_dir = "includes/images"
 javascripts_dir = "includes/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :compact
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
