ab184aea3ad44e356e6f0f44740acc37c05ba41f
Sebin Song <songxevin@gmail.com>
2023-09-07 02:16:39 +0900

    #56 convert project to astro website (#65)
    * new folder structure / basic project configuration / create Base astro components for layout
    
    * create global scss and import it to BaseHead.astro
    
    * work on index.astro / create Ribbon.astro
    
    * make use of path alias feature of astro
    
    * some css to scss in index.astro
    
    * move project font files and update _typography.scss
    
    * create Header.astro / centralise Ribbon.astro toggle / create dpki.astro page component
    
    * create donate.astro / fix some UI bugs in there
    
    * work on team.astro and organise its scss
    
    * move some static assets to /public folder, update image paths in /team page
    
    * update image paths in index.astro / test 'astro preview' command
    
    * create finances.astro page
    
    * add browser-extension.astro / make sure the slide-show is not broken
    
    * make sure the image paths are correct in /browser-extension page
    
    * remove legacy folder & files from the root directory
    
    * remove lagacy pkgs and server.js
    
    * remove legacy script commands

---
+++ astro.config.mjs
@@ -0,0 +1,6 @@
+import { defineConfig } from 'astro/config'
+
+// reference: https://docs.astro.build/en/reference/configuration-reference/
+export default defineConfig({
+  site: 'https://okturtles.org'
+})
