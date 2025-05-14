fe70074c95e195d0ab99e737f43b4d47c40602d5
SebinSong <songxevin@gmail.com>
2025-05-14 13:56:38 +0900

    build ModalClose.vue / boilerplate ModalTemplate.vue

---
+++ src/components/modal/ModalClose.vue
@@ -0,0 +1,64 @@
+<template>
+<button class="c-modal-close"
+  type="button"
+  @click.stop="onClick">
+  <span class="c-close-bar"></span>
+  <span class="close-bar"></span>
+</button>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits<{
+  (e: 'close'): void
+}>()
+
+const onClick = () => {
+  emit('close')
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../styles/variables.scss";
+
+.c-modal-close {
+  position: relative;
+  display: inline-block;
+  background-color: $white;
+  color: $black;
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  border: 1px solid rgba(0, 0, 0, 0);
+  cursor: pointer;
+  transition:
+    background-color 0.3s ease-out,
+    border-color 0.3s ease-out;
+
+  &:hover,
+  &focus {
+    background-color: $background-grey;
+  }
+
+  &:focus {
+    border-color: $grey_3;
+  }
+
+  .c-close-bar {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 24px;
+    height: 2px;
+    border-radius: 2px;
+    background-color: currentColor;
+
+    &:first-child {
+      transform: translateX(-50%) rotate(45deg);
+    }
+
+    &:last-child {
+      transform: translateX(-50%) rotate(-45deg);
+    }
+  }
+}
+</style>
--- src/components/modal/ModalTemplate.vue
+++ src/components/modal/ModalTemplate.vue
@@ -6,19 +6,37 @@
     <header class='c-modal-header'>
       <h1 v-if="title">{title}</h1>
 
-      <button></button>
+      <modal-close class="c-close-btn" @close="closeModal" />
     </header>
+
+    <section class="c-modal-body">
+      <slot></slot>
+    </section>
+
+    <footer v-if="$slots.footer" class="c-modal-footer">
+      <slot name="footer"></slot>
+    </footer>
   </div>
 </div>
 </template>
 
 <script setup lang="ts">
+import ModalClose from './ModalClose.vue'
+
 interface ComponentProps {
+  modalName: string, // NOTE: required prop. 
   title?: string
   hideClose?: boolean
 }
 
 const props = defineProps<ComponentProps>()
+
+const closeModal = () => {}
+
+// define expose
+defineExpose({
+  closeModal
+})
 </script>
 
 <style lang="scss" scoped>
