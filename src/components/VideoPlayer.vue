d99c8b0a36170b99431c2e14d0a06d9ec2e3e4ab
SebinSong <songxevin@gmail.com>
2025-06-09 17:32:58 +0900

    add plyr to the project / bui;d VideoPlayer.vue

---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="video-player-container plyr_override">
+  <video ref="video-el" playsinline controls>
+    <source :src="src" :type="mimeType" />
+  </video>
+</div>
+</template>
+
+<script setup lang="ts">
+import { useTemplateRef, ref, onMounted } from 'vue'
+import Plyr, { type Options as PlyrOptions } from 'plyr'
+
+interface ComponentProps {
+  src: string,
+  mimeType: string
+}
+
+const props = defineProps<ComponentProps>()
+
+const videoEl = useTemplateRef('video-el')
+const player = ref<any>(null)
+
+// methods
+const initPlayer = () => {
+  const opts: PlyrOptions = { debug: false }
+
+  player.value = new Plyr(videoEl.value as HTMLElement, opts)
+}
+
+onMounted(() => {
+  initPlayer()
+})
+</script>
+
+<style lang="scss" src="../styles/plyr/_plyr_override.scss"></style>
