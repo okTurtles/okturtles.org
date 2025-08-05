df0e6f6daa1cda3121126d223a836771e52012ed
SebinSong <songxevin@gmail.com>
2025-08-06 09:42:12 +1200

    add a link with an icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import astroVue from '@astrojs/vue'
 
 // reference: https://docs.astro.build/en/reference/configuration-reference/
 export default defineConfig({
+  devToolbar: {
+    enabled: false
+  },
   site: 'https://okturtles.org',
   integrations: [astroVue()]
 })
