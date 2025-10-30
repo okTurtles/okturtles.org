2120ba453aa4f1dbc3118988cd294d49efa37ddd
SebinSong <songxevin@gmail.com>
2025-10-31 10:31:53 +1300

    initial solution

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script setup lang="ts">
-import { useTemplateRef, ref, onMounted } from 'vue'
+import { useTemplateRef, ref, onMounted, onBeforeUnmount } from 'vue'
 interface ComponentProps {
   src: string,
   mimeType: string,
@@ -18,8 +18,22 @@ const props = defineProps<ComponentProps>()
 
 const videoEl = useTemplateRef('video-el')
 const player = ref<any>(null)
+const isPhone = ref<boolean | null>(null)
+const isPhoneMatchMedia = ref<any>(null)
 
 // methods
+const onIsPhoneChange = (event: MediaQueryListEvent) => {
+  isPhone.value = event.matches
+}
+
+const adjustOrientation = ({ toLandscape = false } = {}) => {
+  if (isPhone.value) {
+    const orientation = screen?.orientation as any
+    if (orientation && 'lock' in orientation) {
+      orientation.lock(toLandscape ? 'landscape' : 'any')
+    }
+  }
+}
 const initPlayer = () => {
   const opts = {
     // Documentation for plyr options: https://www.npmjs.com/package/plyr#options
@@ -33,10 +47,27 @@ const initPlayer = () => {
   }
 
   player.value = new Plyr(videoEl.value as HTMLElement, opts)
+
+  // plyr event handlers
+  player.value.on('enterfullscreen', () => {
+    adjustOrientation({ toLandscape: true })
+  })
+  player.value.on('exitfullscreen', () => {
+    adjustOrientation({ toLandscape: false })
+  })
+
+  // browser event handlers
+  isPhoneMatchMedia.value = window.matchMedia('(hover: none) and (pointer: coarse) and (max-width: 760px)')
+  isPhone.value = isPhoneMatchMedia.value.matches
+  isPhoneMatchMedia.value.addEventListener('change', onIsPhoneChange)
 }
 
 onMounted(() => {
   initPlayer()
+})
+
+onBeforeUnmount(() => {
+  isPhoneMatchMedia.value.removeEventListener('change', onIsPhoneChange)
 })
 </script>
 
