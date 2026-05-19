b2c6aca159a43c57978e677e3b2a4980d2521b90
SebinSong <songxevin@gmail.com>
2026-05-20 11:01:58 +1200

    fix

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script setup lang="ts">
-import { useTemplateRef, ref, onMounted } from 'vue'
+import { useTemplateRef, ref, onMounted, onBeforeUnmount } from 'vue'
 interface ComponentProps {
   src: string,
   mimeType: string,
@@ -18,8 +18,20 @@ const props = defineProps<ComponentProps>()
 
 const videoEl = useTemplateRef('video-el')
 const player = ref<any>(null)
+const isPhone = ref<boolean>(false)
+let isPhoneMatchMedia: any = null
 
 // methods
+const adjustDeviceOrientation = (toLandscape: boolean = false) => {
+  const orientationHandle = screen?.orientation as any
+  if (isPhone.value && orientationHandle && 'lock' in orientationHandle) {
+    const valTo = toLandscape ? 'landscape' : 'any'
+    orientationHandle.lock(valTo).catch((err: any) => {
+      console.error('Failed to lock screen orientation', err)
+    })
+  }
+}
+
 const initPlayer = () => {
   const opts = {
     // Documentation for plyr options: https://www.npmjs.com/package/plyr#options
@@ -33,10 +45,30 @@ const initPlayer = () => {
   }
 
   player.value = new Plyr(videoEl.value as HTMLElement, opts)
+
+  // Event listeners
+  player.value.on('enterfullscreen', () => {
+    adjustDeviceOrientation(true)
+  })
+  player.value.on('exitfullscreen', () => {
+    adjustDeviceOrientation(false)
+  })
 }
 
 onMounted(() => {
   initPlayer()
+
+  isPhoneMatchMedia = matchMedia('(hover: none) and (pointer: coarse) and (max-width: 768px)')
+  isPhoneMatchMedia.onchange = () => {
+    isPhone.value = isPhoneMatchMedia.matches
+  }
+  isPhone.value = isPhoneMatchMedia.matches
+})
+
+onBeforeUnmount(() => {
+  if (isPhoneMatchMedia) {
+    isPhoneMatchMedia.onchange = null
+  }
 })
 </script>
 
